5faa336aee1f5c8efe283b28197cea5c58e0c871
jani.maenpaa <jani.maenpaa@aalto.fi>
2024-02-16 13:09:57 +0200

    Add getNationById util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Nation } from "./types";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getNationById(
+  nations: Nation[],
+  id: number,
+): Nation | undefined {
+  const nation = nations.find((nation) => nation.id === id);
+  return nation;
 }
