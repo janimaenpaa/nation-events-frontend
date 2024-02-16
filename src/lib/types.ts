0068fca34b10af8fb668b54c17d8aff7744eb232
jani.maenpaa <jani.maenpaa@aalto.fi>
2024-02-16 13:09:28 +0200

    Add types

---
+++ src/lib/types.ts
@@ -0,0 +1,20 @@
+export type Nation = {
+    abbreviation: string
+    customCalendarUrl: string | null
+    events: Event[]
+    googleCalendarId: string | null
+    icalUrl: string | null
+    id: number
+    kideUrl: string | null
+    name: string
+    url: String
+  }
+  
+  export type Event = {
+    description: string | null
+    endTime: string
+    startTime: string
+    name: string
+    url: string
+  }
+  
