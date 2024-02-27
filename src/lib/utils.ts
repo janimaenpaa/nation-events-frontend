b747bd4258b3e6d70c92b843102b781657383460
jani.maenpaa <jani.maenpaa@aalto.fi>
2024-02-27 16:02:11 +0200

    Add function for returning the name of the weekday from date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { Nation } from "./types";
+import { isToday, isTomorrow, format } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -12,4 +13,16 @@ export function getNationById(
 ): Nation | undefined {
   const nation = nations.find((nation) => nation.id === id);
   return nation;
+}
+
+export function getDate(date: string) {
+  const day = new Date(date);
+
+  if (isToday(day)) {
+    return "Today";
+  } else if (isTomorrow(day)) {
+    return "Tomorrow";
+  } else {
+    return format(day, "EEEE");
+  }
 }
