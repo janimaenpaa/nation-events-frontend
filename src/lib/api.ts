68249a495110b312032fac994f754fcbc355c160
jani.maenpaa <jani.maenpaa@aalto.fi>
2024-03-25 13:44:22 +0200

    Add return types to functions

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,9 @@
 import { env } from "@/env";
+import { Event, Nation } from "./types";
 
 const API_URL = env.NEXT_PUBLIC_API_URL;
 
-export async function getEventsData() {
+export async function getEventsData(): Promise<Event[]> {
   const response = await fetch(`${API_URL}/events`);
 
   if (!response.ok) {
@@ -14,7 +15,7 @@ export async function getEventsData() {
   return data;
 }
 
-export async function getNationData() {
+export async function getNationData(): Promise<Nation[]> {
   const response = await fetch(`${API_URL}/nations`);
 
   if (!response.ok) {
@@ -26,7 +27,7 @@ export async function getNationData() {
   return data;
 }
 
-export async function getThisWeeksEvents() {
+export async function getThisWeeksEvents(): Promise<Event[]>{
   const response = await fetch(`${API_URL}/events/this-week`);
 
   if (!response.ok) {
@@ -37,3 +38,18 @@ export async function getThisWeeksEvents() {
 
   return data;
 }
+
+export async function getEventData(id: string): Promise<Event> {
+    const response = await fetch(`${env.NEXT_PUBLIC_API_URL}/events/${id}`, {
+      cache: "no-store",
+    });
+  
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+  
+    const { data } = await response.json();
+  
+    return data;
+  }
+  
